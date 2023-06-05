6fc25f0ff49bcb66245c40cfd1ad813593221429
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-06-05 10:54:45 -0300

    layout form notas

--- app/src/main/java/com/example/rickynotas/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rickynotas/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rickynotas
+package com.example.rickynotas.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rickynotas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
