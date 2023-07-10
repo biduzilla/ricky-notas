c9ce8bfde1724b9c5ba8bbc3ce6a6cf0dad9b1ad
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-07-10 11:32:50 -0300

    notas repositorio

---
+++ app/src/main/java/com/example/momonotes/repository/NotaRepository.kt
@@ -0,0 +1,18 @@
+package com.example.momonotes.repository
+
+import com.example.momonotes.data.dao.NotaDatabaseDao
+import com.example.momonotes.model.Nota
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+
+import javax.inject.Inject
+
+class NotaRepository @Inject constructor(private val notaDao: NotaDatabaseDao) {
+    suspend fun addNota(nota: Nota) = notaDao.insertNota(nota)
+    suspend fun updateNota(nota: Nota) = notaDao.updateNota(nota)
+    suspend fun deleteNota(nota: Nota) = notaDao.deleteNota(nota)
+    suspend fun deleteAll() = notaDao.deleteAll()
+    fun getAllNotas(): Flow<List<Nota>> = notaDao.getAll().flowOn(Dispatchers.IO).conflate()
+}
